52810df26b1981cb5123b7324515ea17a2b942da
Dytser <100004773+Dytser@users.noreply.github.com>
2026-03-27 16:11:32 +0100

    Added Initial Bone maps

---
+++ bone_maps/draenei_Fem.py
@@ -0,0 +1,89 @@
+LABEL = "Draenei"
+TYPE = "Character"       # REQUIRED
+SUBTYPE = "Female"      # OPTIONAL (can be None)
+
+BONES = [
+    #### Core Bones ####
+    ("Bone001", "Root"),
+    ## Upper Body ##
+    ("Bone003", "Spine_1"),
+    ("Bone011", "Spine_2"),
+    ("Bone019", "Spine_3"),
+    ("Bone031", "Neck1"),
+    ("Bone041", "Neck2"),
+    ("Bone052", "Head"),
+    # Left Arms
+    ("Bone030", "Shoulder_L"),
+    ("Bone040", "Arm_L"),
+    ("Bone049", "ForeArm_L"),
+    ("Bone057", "Hand_L"),
+    # Right Arms
+    ("Bone027", "Shoulder_R"),
+    ("Bone038", "Arm_R"),
+    ("Bone045", "ForeArm_R"),
+    ("Bone055", "Hand_R"),
+
+    ## Bend Arms
+    # Arms_L Bend
+    ("Bone051", "Bicep_L"),
+    ("Bone050", "Elbow_L"),
+    ("Bone058", "Wank_L"),
+    ("Bone059", "Wrist_L"),
+    # Arms_R Bend
+    ("Bone046", "Bicep_R"),
+    ("Bone047", "Elbow_R"),
+    ("Bone053", "Wank_R"),
+    ("Bone054", "Wrist_R"),
+
+    # Breasts
+    ("Bone028", "Breast_L"),
+    ("Bone032", "Breast_R"),
+
+
+    ### Lower body ##
+    ## Core Legs
+    ("Bone002", "Pelvis"),
+    # Left Side
+    ("Bone006", "Thigh_L"),
+    ("Bone015", "Calf_L"),
+    ("Bone023", "Hoof_L"),
+    ("Bone036", "Foot_L"),
+    ("Bone043", "Toe_L"),
+    # Right Side
+    ("Bone005", "Thigh_R"),
+    ("Bone014", "Calf_R"),
+    ("Bone022", "Hoof_R"),
+    ("Bone034", "Foot_R"),
+    ("Bone042", "Toe_R"),
+
+    ## Bend Legs
+    # Leg_L Bend
+    ("Bone016", "Knee_L"),
+    ("Bone024", "HoofBend_L"),
+    ("Bone035", "Ankle_L"),
+
+
+    # Leg_R Bend
+    ("Bone013", "Knee_R"),
+    ("Bone021", "HoofBend_R"),
+    ("Bone033", "Ankle_R"),
+
+    ### Extra Bones ###
+    ## Tabard
+    # Front
+    ("Bone004", "FrontTabard_1"),
+    ("Bone012", "FrontTabard_2"),
+    ("Bone020", "FrontTabard_3"),
+    # Back
+    ("Bone007", "BackTabard_1"),
+    ("Bone017", "BackTabard_2"),
+    ("Bone025", "BackTabard_3"),
+
+    ## Cape
+    ("Bone029", "Cape_1"),
+    ("Bone039", "Cape_2"),
+    ("Bone048", "Cape_3"),
+    ("Bone056", "Cape_4"),
+    ("Bone108", "Cape_5"),
+    # ...
+]
---
+++ bone_maps/nelf_Fem.py
@@ -0,0 +1,84 @@
+LABEL = "Night Elf"
+TYPE = "Character"       # REQUIRED
+SUBTYPE = "Female"      # OPTIONAL (can be None)
+
+BONES = [
+    #### Core Bones ####
+    ("Bone001", "Root"),
+    ## Upper Body ##
+    ("Bone003", "Spine_1"),
+    ("Bone009", "Spine_2"),
+    ("Bone014", "Spine_3"),
+    ("Bone025", "Neck1"),
+    ("Bone032", "Neck2"),
+    ("Bone042", "Head"),
+    # Left Arms
+    ("Bone022", "Shoulder_L"),
+    ("Bone030", "Arm_L"),
+    ("Bone038", "ForeArm_L"),
+    ("Bone048", "Hand_L"),
+    # Right Arms
+    ("Bone021", "Shoulder_R"),
+    ("Bone029", "Arm_R"),
+    ("Bone035", "ForeArm_R"),
+    ("Bone047", "Hand_R"),
+
+    ## Bend Arms
+    # Arms_L Bend
+    ("Bone040", "Bicep_L"),
+    ("Bone039", "Elbow_L"),
+    ("Bone049", "Wank_L"),
+    ("Bone046", "Wrist_L"),
+    # Arms_R Bend
+    ("Bone037", "Bicep_R"),
+    ("Bone036", "Elbow_R"), 
+    ("Bone045", "Wank_R"),
+    ("Bone046", "Wrist_R"),
+
+    # Breasts
+    ("Bone023", "Breast_L"),
+    ("Bone026", "Breast_R"),
+
+
+    ### Lower body ##
+    ## Core Legs
+    ("Bone002", "Pelvis"),
+    # Left Side
+    ("Bone013", "Thigh_L"),
+    ("Bone019", "Calf_L"),
+    ("Bone034", "Foot_L"),
+    ("Bone044", "Toe_L"),
+    # Right Side
+    ("Bone011", "Thigh_R"),
+    ("Bone017", "Calf_R"),
+    ("Bone033", "Foot_R"),
+    ("Bone043", "Toe_R"),
+
+    ## Bend Legs
+    # Leg_L Bend
+    ("Bone020", "Knee_L"),
+    ("Bone028", "Ankle_L"),
+
+    # Leg_R Bend
+    ("Bone016", "Knee_R"),
+    ("Bone027", "Ankle_R"),
+
+    ### Extra Bones ###
+    ## Tabard
+    # Front
+    ("Bone004", "FrontTabard_1"),
+    ("Bone010", "FrontTabard_2"),
+    ("Bone015", "FrontTabard_3"),
+    # Back
+    ("Bone006", "BackTabard_1"),
+    ("Bone012", "BackTabard_2"),
+    ("Bone018", "BackTabard_3"),
+
+    ## Cape
+    ("Bone024", "Cape_1"),
+    ("Bone031", "Cape_2"),
+    ("Bone041", "Cape_3"),
+    ("Bone052", "Cape_4"),
+    ("Bone112", "Cape_5"),
+    # ...
+]
